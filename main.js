562f54fc349086e6795871edc4453b6eb350b2d0
alexdumitruu <alex.dumitru.g@gmail.com>
2025-09-08 11:26:40 +0300

    step 2

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+// console.log('Hello World!');
+
+const myLibrary = [];
+
+function Book(name, author) {
+    this.name = name;
+    this.author = author;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(name, author) {
+    let b = new Book(name, author);
+    myLibrary.push(b);
+}
+
+// addBookToLibrary("Alex", "Dumitru");
+// console.log(myLibrary);
+
+
+
